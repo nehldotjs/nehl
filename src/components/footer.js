5b88736fb124485c92e2682c639816f34ec1fbf6
nehldotjs <nelsonosuya11@gmail.com>
2023-01-22 12:55:53 +0100

    NEW COMMIT

---
+++ src/components/footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <footer>
+        <div className="footer">
+          <h3>
+            © 2023 |<span className="f-logo"> NEHL  </span> ---Designed and
+            styled by
+            <span>ME!</span>
+          </h3>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
